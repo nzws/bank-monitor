e0aacc13b59a5dc4b355ae377204c5e1f2cebea9
BSKY <git@bsky.moe>
2020-03-10 06:40:13 +0900

    Add react version section to eslint-plugin-react setting (#7)

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['@yuzulabo', 'plugin:react/recommended']
+  extends: ['@yuzulabo', 'plugin:react/recommended'],
+  settings: { react: { version: 'detect' } }
 };
