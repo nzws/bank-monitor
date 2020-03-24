29e1bc87aafc2d194d0e3fc830ecafbb23c731c3
nzws <git@nzws.me>
2020-03-24 12:32:00 +0900

    fix

--- packages/backend/app/controllers/api/deposit_rakuten.js
+++ packages/backend/app/controllers/api/deposit_rakuten.js
@@ -5,6 +5,8 @@ import state from '../../utils/state';
 import { notificationSender } from '../../utils/notification';
 import { logError } from '../../utils/logger';
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 const apiDepositRakuten = async ctx => {
   const { bankId, amount } = ctx.request.body;
   const UID = ctx.state.auth.UID;
@@ -30,6 +32,7 @@ const apiDepositRakuten = async ctx => {
 
     await session.init(browser);
     await session.login(username, password, options);
+    await sleep(3000);
 
     const logs = await session.action('DEPOSIT_FROM_JPBANK', {
       amount,
